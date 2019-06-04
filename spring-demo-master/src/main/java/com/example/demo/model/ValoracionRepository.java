8fff42e71bcac421dea74c5027580d149c342dd8
OmegaWolfy <48200949+OmegaWolfy@users.noreply.github.com>
2019-06-04 11:32:09 +0200

    Delete ValoracionRepository.java

--- spring-demo-master/src/main/java/com/example/demo/model/ValoracionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValoracionRepository extends JpaRepository<Valoracion, String> {
-}
