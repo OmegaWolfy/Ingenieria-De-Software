fd72b767bae565614206df1108a62c95c049713e
OmegaWolfy <48200949+OmegaWolfy@users.noreply.github.com>
2019-06-04 11:28:28 +0200

    Delete AlertasRepository.java

--- spring-demo-master/src/main/java/com/example/demo/model/AlertasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertasRepository extends JpaRepository<Alertas, String> {
-	Alertas findBytrafico(String idalertas);
-
-	
-}
